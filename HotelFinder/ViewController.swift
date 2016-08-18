bf424c0b07d985bad874c0f4b8c0f9bab4aada75
Roberto Merino <rob@robertos-mac.local>
2016-08-18 14:45:57 -0400

    Initial Commit

---
+++ HotelFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotelFinder
+//
+//  Created by Roberto Merino on 18/8/16.
+//  Copyright © 2016 Roberto Merino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
